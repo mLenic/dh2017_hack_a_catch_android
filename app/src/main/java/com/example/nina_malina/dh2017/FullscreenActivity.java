0c322a55bf59a5f1d09d69e4f2b4d9254ead7afe
Matevz Lenic <matevz.lenic@gmail.com>
2017-05-21 10:27:30 +0200

    Some color and strings fixes

--- app/src/main/java/com/example/nina_malina/dh2017/FullscreenActivity.java
+++ app/src/main/java/com/example/nina_malina/dh2017/FullscreenActivity.java
@@ -44,7 +44,7 @@ import java.net.URL;
 
 public class FullscreenActivity extends AppCompatActivity{
     double ax,ay,az;
-    private int mInterval = 100; // 0.1 seconds
+    private int mInterval = 250; // 0.1 seconds
     private Handler mHandler;
     float my_x;
     float my_y;
